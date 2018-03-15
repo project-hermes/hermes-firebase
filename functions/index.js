c90b6685a8b3026afac2f2d2b469cfbd9e6d7c5c
Bradford Bazemore <bankersalgo@gmail.com>
2018-03-14 23:49:53 -0400

    can now push to firestore

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,27 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const atob = require('atob');
+var moment = require('moment');
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Hello from Firebase!");
+});
+
+exports.helloPubSub = functions.pubsub.topic('test').onPublish((event) => {
+    var db = admin.firestore();
+    var now = moment().unix();
+    var docRef = db.collection('hermes1').doc(now.toString());
+    const pubSubMessage = event.data;
+    try {
+        let data = atob(pubSubMessage.data);
+        console.log(data);
+        docRef.set({
+            'data': data
+        });
+    } catch (e) {
+        console.error('PubSub message error', e);
+        return 1;
+    }
+    return 0;
+});
