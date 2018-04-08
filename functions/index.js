d7100cfd2b116ff7898e0a4575dc461f7f5221ca
Bradford Bazemore <bankersalgo@gmail.com>
2018-04-08 11:43:13 -0400

    init vuestic

--- functions/index.js
+++ functions/index.js
@@ -4,21 +4,26 @@ const atob = require('atob');
 var moment = require('moment');
 admin.initializeApp(functions.config().firebase);
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
-});
-
 exports.helloPubSub = functions.pubsub.topic('test').onPublish((event) => {
     var db = admin.firestore();
     var now = moment().unix();
-    var docRef = db.collection('hermes1').doc(now.toString());
     const pubSubMessage = event.data;
     try {
         let data = atob(pubSubMessage.data);
-        console.log(data);
-        docRef.set({
-            'data': data
-        });
+        let splitData = data.split(' ');
+        console.log(splitData);
+        db.collection('hermes1').add({
+                time: parseInt(splitData[0]),
+                temperature: parseInt(splitData[1]),
+                depth: parseInt(splitData[2])
+            })
+            .then((docRef) => {
+                console.log("Document written with ID: ", docRef.id);
+                return docRef.id;
+            })
+            .catch((error) => {
+                console.error("Error adding document: ", error);
+            });
     } catch (e) {
         console.error('PubSub message error', e);
         return 1;
---
+++ src/components/layout/Layout.vue
@@ -0,0 +1,92 @@
+<template>
+  <div :class="classObject" v-resize>
+    <navbar></navbar>
+    <sidebar></sidebar>
+
+    <div class="content-wrap" id="content-wrap">
+      <main id="content" class="content" role="main">
+        <vuestic-breadcrumbs :breadcrumbs="breadcrumbs"/>
+        <vuestic-pre-loader v-show="isLoading" ref="preLoader" class="pre-loader"></vuestic-pre-loader>
+        <router-view v-show="!isLoading"></router-view>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+  import {mapGetters} from 'vuex'
+
+  import Navbar from './navbar/Navbar'
+  import Sidebar from './sidebar/Sidebar'
+  import Resize from 'directives/ResizeHandler'
+
+  export default {
+    name: 'layout',
+
+    components: {
+      Navbar,
+      Sidebar
+    },
+    directives: {
+      resize: Resize
+    },
+    computed: {
+      ...mapGetters([
+        'sidebarOpened',
+        'toggleWithoutAnimation',
+        'isLoading'
+      ]),
+      classObject: function () {
+        return {
+          'sidebar-hidden': !this.toggleWithoutAnimation && !this.sidebarOpened,
+          'sidebar-hidden sidebar-hidden_without-animation': this.toggleWithoutAnimation && !this.sidebarOpened
+        }
+      },
+      breadcrumbs () {
+        return this.$store.getters.breadcrumbs(this.$route.name)
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import "../../sass/_variables.scss";
+  @import "~bootstrap/scss/mixins/breakpoints";
+  @import "~bootstrap/scss/functions";
+  @import "~bootstrap/scss/variables";
+
+  .content-wrap {
+    margin-left: $content-wrap-ml;
+    padding: $content-wrap-pt $content-wrap-pr $content-wrap-pb 0;
+    transition: margin-left 0.3s ease;
+
+    .pre-loader {
+      position: absolute;
+      left: $vuestic-preloader-left;
+      top: $vuestic-preloader-top;
+    }
+
+    .sidebar-hidden & {
+      margin-left: $sidebar-left;
+    }
+
+    @include media-breakpoint-down(md) {
+      padding: $content-mobile-wrap;
+      margin-left: 0;
+
+      .sidebar-hidden & {
+        margin-left: 0;
+      }
+    }
+  }
+
+  .made-by-footer {
+    position: absolute;
+    bottom: 0;
+    height: calc(#{$layout-padding} + #{$widget-mb});
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
