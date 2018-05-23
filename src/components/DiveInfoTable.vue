8578013ebf68751c588031196bcd636afc20c7df
Dalton Johnson <dalton@pendo.io>
2018-05-22 21:48:56 -0400

    ugly loop to calc nums and build series

---
+++ src/components/DiveInfoTable.vue
@@ -0,0 +1,34 @@
+<template>
+    <el-table
+      :data="info"
+      style="width: 100%">
+      <el-table-column
+        prop="prop"
+        label="Parameter"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="max"
+        label="Max"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="min"
+        label="Min">
+      </el-table-column>
+      <el-table-column
+        prop="avg"
+        label="Avg">
+      </el-table-column>
+    </el-table>
+</template>
+
+<script>
+    export default {
+        props: {
+            info: {
+                type: Array
+            }
+        }
+    }
+</script>
--- src/pages/EasyAccess.vue
+++ src/pages/EasyAccess.vue
@@ -4,12 +4,14 @@
         :dives="dives"
         :onChange="onDiveSelect"
       />
+      <DiveInfoTable :info="diveInfo" />
       <Chart :chart-data="chartData" />
   </div>
 </template>
 
 <script>
 import DiveChooser from '../components/DiveChooser.vue';
+import DiveInfoTable from '../components/DiveInfoTable.vue';
 import Chart from '../components/Chart.vue';
 import sortBy from 'lodash/sortBy';
 import {db} from '../firebase';
@@ -17,12 +19,14 @@ import {db} from '../firebase';
 export default {
     components: {
         DiveChooser,
+        DiveInfoTable,
         Chart
     },
     data () {
         return {
             dives: [],
-            chartData: []
+            chartData: [],
+            diveInfo: []
         };
     },
     mounted () {
@@ -49,25 +53,80 @@ export default {
             });
         },
         onDiveSelect (id) {
-            db.doc(`Dive/${id}`).collection('data').get().then(snapshot => {
-                const rows = sortBy(snapshot.docs, ['id']).map(row => {
-                    const data = row.data();
-                    return {
-                        x: data.timestamp,
-                        y: data.temp1
-                    };
+            this.fetchDive(id).then(snapshot => {
+                const rows = sortBy(snapshot.docs, ['id']);
+
+                const depthInfo = {
+                    prop: 'depth',
+                    min: Number.MAX_SAFE_INTEGER,
+                    max: Number.MIN_SAFE_INTEGER,
+                    sum: 0
+                };
+                const depthSeries = [];
+
+                const temp1Info = {
+                    prop: 'temp1',
+                    min: Number.MAX_SAFE_INTEGER,
+                    max: Number.MIN_SAFE_INTEGER,
+                    sum: 0
+                };
+                const temp1Series = [];
+
+                const temp2Info = {
+                    prop: 'temp2',
+                    min: Number.MAX_SAFE_INTEGER,
+                    max: Number.MIN_SAFE_INTEGER,
+                    sum: 0
+                };
+                const temp2Series = [];
+
+                const chartTimeStampLabels = [];
+
+                rows.forEach(row => {
+                    const {depth, temp1, temp2, timestamp} = row.data();
+                    depthSeries.push({x: timestamp, y: depth});
+                    depthInfo.sum += depth;
+                    if (depthInfo.min > depth) depthInfo.min = depth;
+                    if (depthInfo.max < depth) depthInfo.max = depth;
+
+                    temp1Series.push({x: timestamp, y: temp1});
+                    temp1Info.sum += temp1;
+                    if (temp1Info.min > temp1) temp1Info.min = temp1;
+                    if (temp1Info.max < temp1) temp1Info.max = temp1;
+
+                    temp2Series.push({x: timestamp, y: temp2});
+                    temp2Info.sum += temp2;
+                    if (temp2Info.min > temp2) temp2Info.min = temp2;
+                    if (temp2Info.max < temp2) temp2Info.max = temp2;
+
+                    chartTimeStampLabels.push((new Date(timestamp * 1000)).toTimeString());
                 });
 
+                depthInfo.avg = depthInfo.sum / rows.length;
+                temp1Info.avg = temp1Info.sum / rows.length;
+                temp2Info.avg = temp2Info.sum / rows.length;
+
                 this.chartData = {
-                    labels: rows.map(row => (new Date(row.x * 1000)).toTimeString()),
+                    labels: chartTimeStampLabels,
                     datasets: [
-                        {
-                            label: 'Temperature',
-                            data: rows
-                        }
+                        // {label: 'Depth', data: depthSeries, backgroundColor: 'red'},
+                        {label: 'Temp 1', data: temp1Series, backgroundColor: 'green'},
+                        // {label: 'Temp 2', data: temp2Series, backgroundColor: 'blue'}
                     ]
-                };
+                }
+
+                this.diveInfo = [
+                    depthInfo,
+                    temp1Info,
+                    temp2Info
+                ];
             });
+        },
+        fetchDive (id) {
+            return db.doc(`Dive/${id}`).collection('data').get();
+        },
+        getNums (rows) {
+
         }
     }
 }
