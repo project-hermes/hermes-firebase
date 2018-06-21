17d3686fa95a9883ab617acc0fb3b40e336fe39f
Dalton Johnson <dalton@pendo.io>
2018-06-20 22:21:10 -0400

    add openstreetmap cred

--- src/components/SimpleMap/SimpleMap.vue
+++ src/components/SimpleMap/SimpleMap.vue
@@ -5,6 +5,11 @@
 <script>
 import L from 'leaflet';
 import head from 'lodash/head';
+import {
+  GeoSearchControl,
+  OpenStreetMapProvider,
+} from 'leaflet-geosearch';
+
 export default {
     props: {
         markers: {
@@ -25,6 +30,7 @@ export default {
     },
     mounted() {
         this.initMap();
+        this.map.setView([0, 0], 2);
         if (this.markers.length) {
             this.replaceMarkers(this.markers);
         }
@@ -33,9 +39,27 @@ export default {
         initMap() {
             this.map = L.map('simple-map');
             L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                maxZoom: 18
+                maxZoom: 18,
+                attribution:
+                    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
             }).addTo(this.map);
             this.map.scrollWheelZoom.disable();
+
+            // var bounds = [[54.559322, -5.767822], [56.1210604, -3.021240]];
+            // // create an orange rectangle
+            // L.rectangle(bounds, {color: "#ff7800", weight: 1}).addTo(this.map);
+            // // zoom the map to the rectangle bounds
+            // this.map.fitBounds(bounds);
+
+            // const provider = new OpenStreetMapProvider();
+            //
+            // const searchControl = new GeoSearchControl({
+            //   provider,
+            //   style: 'bar',
+            //   showMarker: false
+            // });
+            //
+            // this.map.addControl(searchControl);
         },
         removeMarkers(map, markers) {
             markers.forEach(marker => {
