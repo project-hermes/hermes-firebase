5bb0c626dc1e2a07d818a8442aeb41c54fab56a5
Dalton Johnson <dalton@pendo.io>
2018-07-07 11:40:03 -0600

    add MainView

---
+++ src/pages/MainView/MainView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="map">
+    <main class="map__main">
+      <div class="container">
+        <SimpleMap
+          style="height: 800px;"
+        />
+      </div>
+    </main>
+  </div>
+</template>
+<script>
+import {SimpleMap} from '~/components';
+export default {
+    components: {
+        SimpleMap
+    }
+};
+</script>
+<style lang="scss" scoped>
+</style>
